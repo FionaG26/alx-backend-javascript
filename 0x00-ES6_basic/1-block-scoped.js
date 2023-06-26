198155551e85abdb6fa19a5b0b83857de4dcee4c
FionaG26 <fionamuthoni18@gmail.com>
2023-06-26 18:07:31 +0000

    tasks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
-  
-    if (trueOrFalse) {
-      // eslint-disable-next-line
-      const task = true;
-      // eslint-disable-next-line
-      const task2 = false;
-    }
-  
-    return [task, task2];
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    var innerTask = true;
+    var innerTask2 = false;
   }
+
+  return [task, task2];
+}
