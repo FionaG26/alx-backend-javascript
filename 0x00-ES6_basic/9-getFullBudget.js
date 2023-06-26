a796ea75c2b7e09131c002466437bc36b6f3cc67
FionaG26 <fionamuthoni18@gmail.com>
2023-06-26 19:21:11 +0000

    tasks

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,5 +1,3 @@
-import getBudgetObject from './7-getBudgetObject.js';
-
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
