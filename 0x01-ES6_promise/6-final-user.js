08ab78dce084cb3e8d351f71bebcc6eec9b503b2
FionaG26 <fionamuthoni18@gmail.com>
2023-06-27 16:01:41 +0000

    tasks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,16 +2,21 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([promise1, promise2]).then((results) => {
+  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
     return results.map((result) => {
       if (result.status === 'fulfilled') {
-        return { status: result.status, value: result.value };
-      } else {
-        return { status: result.status, value: result.reason.message };
+        return {
+          status: 'fulfilled',
+          value: result.value,
+        };
       }
+      return {
+        status: 'rejected',
+        value: result.reason,
+      };
     });
   });
 }
