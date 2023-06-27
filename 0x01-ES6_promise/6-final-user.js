2093dcd6db84c7ca52f54a5480680944305a46c8
FionaG26 <fionamuthoni18@gmail.com>
2023-06-27 16:20:28 +0000

    tasks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,22 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+async function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
-    return results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.value.status,
-          value: result.value.body || result.value,
-        };
-      }
-      return {
-        status: 'rejected',
-        value: result.reason.message,
-      };
-    });
-  });
+  try {
+    const result = await Promise.allSettled([userPromise, photoPromise]);
+    return result.map((promise) => ({
+      status: promise.status,
+      value: promise.status === 'fulfilled' ? promise.value : promise.reason,
+    }));
+  } catch (error) {
+    throw new Error(error);
+  }
 }
+
+export default handleProfileSignup;
