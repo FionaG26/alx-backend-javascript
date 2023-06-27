301c7db3607686cb70abd64dc1bca3867c259409
FionaG26 <fionamuthoni18@gmail.com>
2023-06-27 15:08:21 +0000

    tasks

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,9 +4,7 @@ function handleResponseFromAPI(promise) {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .catch(() => new Error());
-
-  return promise;
+    .catch(() => new Error())
 }
 
 export default handleResponseFromAPI;
