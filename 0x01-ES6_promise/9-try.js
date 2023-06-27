42e64fbe7330510f56ccac1d04ce7cbf4c8d0f7a
FionaG26 <fionamuthoni18@gmail.com>
2023-06-27 17:36:01 +0000

    tasks

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -6,9 +6,8 @@ export default function guardrail(mathFunction) {
     queue.push(result);
   } catch (error) {
     queue.push(error.message);
-  } finally {
-    queue.push('Guardrail was processed');
   }
 
+  queue.push('Guardrail was processed');
   return queue;
 }
