b75b4fb1c5b8e1db7d4b99ba975b6a7653f44842
FionaG26 <fionamuthoni18@gmail.com>
2023-06-29 09:29:36 +0000

    tasks

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,4 @@
-import ClassRoom from "./0-classroom.js";
-
-function initializeRooms() {
-  const rooms = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34)
-  ];
-  return rooms;
+import ClassRoom from './0-classroom';
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
-
-export default initializeRooms;
