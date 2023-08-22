926f1065abe0e3a5ecf766065236d87cf34a9632
FionaG26 <fionamuthoni18@gmail.com>
2023-08-22 08:47:07 +0000

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,26 +1,13 @@
-// 1-stdin.js
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Check if the script is being run as part of a child process
-const isChildProcess = process.argv.includes('--child');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
 
-// If it's a child process, don't display the welcome message
-if (!isChildProcess) {
-  process.stdout.write('Welcome to Holberton School, what is your name?\n');
-}
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-
-  if (name.length > 0) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  } else {
-    process.stdout.write('Your name is: INPUT\n');
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
+});
 
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-
-  // If it's a child process, exit without the closing message
-  if (!isChildProcess) {
-    process.exit();
-  }
 });
